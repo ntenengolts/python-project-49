43d89b9aab9502bdef7bbf08d3b1d6e317a62d68
Nikolai Tenengolts <ntenengolts@gmail.com>
2024-07-21 23:58:05 +0300

    Update brain_even.py (add is_prime)

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -5,11 +5,17 @@ from brain_games.common import run_game
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+
 def generate_round():
     number = randint(1, 100)
     question = str(number)
-    correct_answer = 'yes' if number % 2 == 0 else 'no'
-    return question, correct_answer
+    correct_answer = 'yes' if is_even(number) else 'no'
+    return question, str(correct_answer)
 
 
 def main():
